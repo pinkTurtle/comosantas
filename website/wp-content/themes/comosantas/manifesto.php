b1a8d7735b77d4a170170aaa654063d75b724044
Facundo Suárez <facundo@svarez.com.ar>
2013-09-06 11:21:05 -0300

    agregando fuentes y estilos

--- website/wp-content/themes/comosantas/manifesto.php
+++ website/wp-content/themes/comosantas/manifesto.php
@@ -8,13 +8,23 @@ Template Name: Manifesto
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
 
 </head>
-<body class="cs pages">
-<div class="cs pages">
-  <div class="cs logo">
+<?php
+
+global $wp_query;
+$post_id = $wp_query->post->ID;
+$post = get_post( $post_id );
+$slug = $post->post_name;
+
+?>
+<body class="pages">
+<div class="pages <?php echo $slug; ?>">
+  <div class="logo <?php echo $slug; ?>">
     <a href="<?php echo get_bloginfo('url'); ?>"></a>
   </div>
 
-  <div class="splash"></div>
+  <?php
+    echo $post->post_content;
+  ?> 
 
 </div>
 

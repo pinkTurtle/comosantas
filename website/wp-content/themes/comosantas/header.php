15cb668947297dd153dc95eac028c739efe14c85
Facundo Suárez <facundo@svarez.com.ar>
2013-09-16 16:38:22 -0300

    ajustando menu, iniciando easing transition

--- website/wp-content/themes/comosantas/header.php
+++ website/wp-content/themes/comosantas/header.php
@@ -32,6 +32,15 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-38598480-3', 'comosantas.com.ar');
+  ga('send', 'pageview');
+</script>
 	<?php wp_head(); ?>
 </head>
 

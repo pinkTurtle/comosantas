7481df6f123e9ab46a00ea03567dcea0a30d35b3
Facundo Suárez <facundo@svarez.com.ar>
2013-09-16 16:05:56 -0300

    ajustando menu, iniciando easing transition

--- website/wp-content/themes/comosantas/404.php
+++ website/wp-content/themes/comosantas/404.php
@@ -3,18 +3,10 @@
 /*
 Template Name: Pagina Como Santas
 */
+get_header();
 ?>
 
-<head>
-  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
-
-</head>
-
-<body class="pages">
 <div class="pages cuatrocerocuatro">
-  <div class="logo cuatrocerocuatro">
-    <a href="<?php echo get_bloginfo('url'); ?>"></a>
-  </div>
   <p>
     Lo sentimos mucho, pero la pagina no pudo ser encontrada
   </p>
